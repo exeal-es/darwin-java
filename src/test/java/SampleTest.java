4ba860b240e0789afff9efb759ac42f6906e13b7
Pedro Pardal <ppardalj@gmail.com>
2024-07-16 12:26:38 +0200

    chore: create project

---
+++ src/test/java/SampleTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SampleTest {
+    @Test
+    public void example() {
+        assertTrue(true);
+    }
+}
