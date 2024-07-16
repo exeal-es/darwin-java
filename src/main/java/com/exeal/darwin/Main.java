09f4b71f9276cd2a9f84fde598b6344b942819da
Pedro Pardal <ppardalj@gmail.com>
2024-07-16 14:36:33 +0200

    refactor: separate read http request logic

--- src/main/java/com/exeal/darwin/Main.java
+++ src/main/java/com/exeal/darwin/Main.java
@@ -36,31 +36,44 @@ public class Main {
         System.out.println("New client connected");
 
         BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        HttpRequest httpRequest = readHttpRequest(in);
+        HttpResponse response = handleResponse(httpRequest);
+
         OutputStream out = clientSocket.getOutputStream();
+        response.writeTo(out);
+    }
 
+    private static HttpRequest readHttpRequest(BufferedReader in) throws IOException {
         String line = in.readLine();
-        if (line != null) {
-            String[] requestParts = line.split(" ");
-            if (requestParts.length > 1) {
-                String verb = requestParts[0];
-                String fullPath = requestParts[1];
-                String[] pathParts = fullPath.split("\\?");
-                String path = pathParts[0];
-                Map<String, String> queryParams = new HashMap<>();
-                if (pathParts.length > 1) {
-                    String[] pairs = pathParts[1].split("&");
-                    for (String pair : pairs) {
-                        String[] kv = pair.split("=");
-                        if (kv.length > 1) {
-                            queryParams.put(kv[0], kv[1]);
-                        }
-                    }
-                }
-                HttpResponse response = handleResponse(new HttpRequest(verb, path, queryParams));
+        if (line == null) {
+            throw new MalformedHttpRequestException("Invalid HTTP request: null line");
+        }
+        String[] requestParts = line.split(" ");
+        if (requestParts.length <= 1) {
+            throw new MalformedHttpRequestException("Invalid HTTP request: " + line);
+        }
+        String verb = requestParts[0];
+        String fullPath = requestParts[1];
+        String[] pathParts = fullPath.split("\\?");
+        String path = pathParts[0];
 
-                response.writeTo(out);
+        Map<String, String> queryParams = readQueryParams(pathParts);
+
+        return new HttpRequest(verb, path, queryParams);
+    }
+
+    private static Map<String, String> readQueryParams(String[] pathParts) {
+        Map<String, String> queryParams = new HashMap<>();
+        if (pathParts.length > 1) {
+            String[] pairs = pathParts[1].split("&");
+            for (String pair : pairs) {
+                String[] kv = pair.split("=");
+                if (kv.length > 1) {
+                    queryParams.put(kv[0], kv[1]);
+                }
             }
         }
+        return queryParams;
     }
 
     private static HttpResponse handleResponse(HttpRequest httpRequest) {
---
+++ src/main/java/com/exeal/darwin/MalformedHttpRequestException.java
@@ -0,0 +1,7 @@
+package com.exeal.darwin;
+
+public class MalformedHttpRequestException extends RuntimeException {
+    public MalformedHttpRequestException(String message) {
+        super(message);
+    }
+}
