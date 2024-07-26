95d82d00e83bdc928669b47716f9c2242c1c4ba6
Pedro Pardal <ppardalj@gmail.com>
2024-07-26 11:44:30 +0200

    refactor: add method statusCode

--- src/main/java/com/exeal/darwin/HttpResponse.java
+++ src/main/java/com/exeal/darwin/HttpResponse.java
@@ -1,31 +1,50 @@
 package com.exeal.darwin;
 
 public class HttpResponse {
-    private final String statusCodeString;
+    // "200 OK"
+    // "201 Created"
+    // "404 Not Found"
+    // "405 Method Not Allowed"
+
+    private final int statusCode;
     private final String body;
 
-    private HttpResponse(String statusCodeString, String body) {
-        this.statusCodeString = statusCodeString;
+    private HttpResponse(int statusCode, String body) {
+        this.statusCode = statusCode;
         this.body = body;
     }
 
     public static HttpResponse ok(String body) {
-        return new HttpResponse("200 OK", body);
+        return new HttpResponse(200, body);
     }
 
     public static HttpResponse created(String body) {
-        return new HttpResponse("201 Created", body);
+        return new HttpResponse(201, body);
     }
 
     public static HttpResponse notFound(String body) {
-        return new HttpResponse("404 Not Found", body);
+        return new HttpResponse(404, body);
     }
 
     public static HttpResponse methodNotAllowed() {
-        return new HttpResponse("405 Method Not Allowed", "");
+        return new HttpResponse(405, "");
     }
 
     public String payload() {
-        return "HTTP/1.1 " + statusCodeString + "\r\n\r\n" + body;
+        return "HTTP/1.1 " + statusCode + " " + statusCodeString() + "\r\n\r\n" + body;
+    }
+
+    private String statusCodeString() {
+        return switch (statusCode) {
+            case 200 -> "OK";
+            case 201 -> "Created";
+            case 404 -> "Not Found";
+            case 405 -> "Method Not Allowed";
+            default -> "";
+        };
+    }
+
+    public int statusCode() {
+        return statusCode;
     }
 }
