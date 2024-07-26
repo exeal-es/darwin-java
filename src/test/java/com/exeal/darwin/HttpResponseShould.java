83a1e8875ce6c22281d35f3b514db2df5c37eb74
Pedro Pardal <ppardalj@gmail.com>
2024-07-26 10:01:40 +0200

    test: better test naming

--- src/test/java/com/exeal/darwin/HttpResponseShould.java
+++ src/test/java/com/exeal/darwin/HttpResponseShould.java
@@ -4,10 +4,10 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class HttpResponseTest {
+public class HttpResponseShould {
 
     @Test
-    public void testPayload() {
+    public void include_http_version() {
         HttpResponse response = new HttpResponse("200 OK\r\n\r\nHello World!");
         assertEquals("HTTP/1.1 200 OK\r\n\r\nHello World!", response.payload());
     }
