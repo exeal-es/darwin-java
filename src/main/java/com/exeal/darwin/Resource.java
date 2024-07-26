359abc7f8fa2557ea35b0c2e049f8f9d200c51d6
Pedro Pardal <ppardalj@gmail.com>
2024-07-26 17:25:39 +0200

    refactor: tell dont ask

--- src/main/java/com/exeal/darwin/Resource.java
+++ src/main/java/com/exeal/darwin/Resource.java
@@ -5,26 +5,35 @@ import java.util.Map;
 
 public final class Resource {
     private final PathTemplate pathTemplate;
-    private final Map<HttpVerb, Handler> map;
+    private final Map<HttpVerb, Handler> handlersByVerb;
 
     public Resource(PathTemplate pathTemplate) {
         this.pathTemplate = pathTemplate;
-        this.map = new HashMap<>();
+        this.handlersByVerb = new HashMap<>();
+    }
+
+    public HttpResponse accept(HttpRequest request) {
+        var handler = handlersByVerb.get(request.verb());
+        if (handler == null) {
+            return HttpResponse.methodNotAllowed();
+        }
+
+        try {
+            return handler.apply(request);
+        } catch (Exception e) {
+            return HttpResponse.internalServerError(e.getMessage());
+        }
+    }
+
+    public boolean hasPathTemplate(PathTemplate pathTemplate) {
+        return this.pathTemplate.equals(pathTemplate);
     }
 
     public boolean matches(String path) {
-        return path().matches(path);
+        return pathTemplate.matches(path);
     }
 
     public void put(HttpVerb verb, Handler handler) {
-        map.put(verb, handler);
-    }
-
-    public Handler get(HttpVerb verb) {
-        return map.get(verb);
-    }
-
-    public PathTemplate path() {
-        return pathTemplate;
+        handlersByVerb.put(verb, handler);
     }
 }
--- src/main/java/com/exeal/darwin/Resources.java
+++ src/main/java/com/exeal/darwin/Resources.java
@@ -2,7 +2,6 @@ package com.exeal.darwin;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.function.Function;
 
 public class Resources {
     private final List<Resource> resources;
@@ -25,33 +24,20 @@ public class Resources {
         if (resource == null) {
             return HttpResponse.notFound("Not Found");
         }
-        var handler = resource.get(request.verb());
-        if (handler == null) {
-            return HttpResponse.methodNotAllowed();
-        }
-
-        try {
-            return handler.apply(request);
-        } catch (Exception e) {
-            return HttpResponse.internalServerError(e.getMessage());
-        }
+        return resource.accept(request);
     }
 
     private Resource findResource(PathTemplate pathTemplate) {
-        for (var resource : resources) {
-            if (resource.path().equals(pathTemplate)) {
-                return resource;
-            }
-        }
-        return null;
+        return resources.stream()
+                .filter(resource -> resource.hasPathTemplate(pathTemplate))
+                .findFirst()
+                .orElse(null);
     }
 
     private Resource findResource(String path) {
-        for (var resource : resources) {
-            if (resource.matches(path)) {
-                return resource;
-            }
-        }
-        return null;
+        return resources.stream()
+                .filter(resource -> resource.matches(path))
+                .findFirst()
+                .orElse(null);
     }
 }
