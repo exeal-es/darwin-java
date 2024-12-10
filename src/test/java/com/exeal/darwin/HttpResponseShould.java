ed707f5877d27ad3805116ebc431a989bffdc58f
Pedro Pardal <ppardalj@gmail.com>
2024-12-10 18:16:19 +0100

    test: fix unit tests

--- src/test/java/com/exeal/darwin/HttpResponseShould.java
+++ src/test/java/com/exeal/darwin/HttpResponseShould.java
@@ -9,18 +9,20 @@ public class HttpResponseShould {
     @Test
     public void create_ok() {
         HttpResponse response = HttpResponse.ok("Hello World!");
-        assertEquals("HTTP/1.1 200 OK\r\n\r\nHello World!", response.payload());
+        assertEquals("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello World!", response.payload());
     }
 
     @Test
     public void create_created() {
         HttpResponse response = HttpResponse.created("Hello World!");
-        assertEquals("HTTP/1.1 201 Created\r\n\r\nHello World!", response.payload());
+        assertEquals("HTTP/1.1 201 Created\r\n" +
+                "Content-Type: text/plain\r\n\r\nHello World!", response.payload());
     }
 
     @Test
     public void create_not_found() {
         HttpResponse response = HttpResponse.notFound("Hello World!");
-        assertEquals("HTTP/1.1 404 Not Found\r\n\r\nHello World!", response.payload());
+        assertEquals("HTTP/1.1 404 Not Found\r\n" +
+                "Content-Type: application/problem+json\r\n\r\n{\"title\":\"Not Found\",\"detail\":\"Resource not found\"}", response.payload());
     }
 }
