4b8c06675ed7760ecfc1932b6910aa67842f9c79
Pedro Pardal <ppardalj@gmail.com>
2024-12-19 19:09:12 +0100

    style: remove dead code

--- src/main/java/com/exeal/darwin/AuthorizationHeaderParser.java
+++ src/main/java/com/exeal/darwin/AuthorizationHeaderParser.java
@@ -2,14 +2,11 @@ package com.exeal.darwin;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
-import com.auth0.jwt.interfaces.JWTVerifier;
-import com.auth0.jwt.algorithms.Algorithm;
 
 import java.util.HashMap;
 import java.util.Map;
 
 public class AuthorizationHeaderParser {
-    private static final String SECRET_KEY = "your_secret_key";
 
     public static ParameterBag parseAuthorizationHeaderAndReturnClaims(String authorizationHeader) {
         if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
