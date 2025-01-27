3c8bcaf14a5b0b571ed5d29aacc35479d9209134
Angel David Macho <admacho@supplynexus.com>
2025-01-27 18:48:21 +0100

    chore: add mockito depency

--- build.gradle.kts
+++ build.gradle.kts
@@ -14,6 +14,7 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("io.rest-assured:rest-assured:5.5.0")
     testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.mockito:mockito-junit-jupiter:5.15.2")
 }
 
 tasks.test {
